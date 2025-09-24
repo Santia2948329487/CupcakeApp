a3b1f1fdbd00523b35167a713ea1128f7eb09e73
Santia2948329487 <santiagocg247@gmail.com>
2025-09-24 16:17:31 -0500

    feat: implementar TopAppBar

--- app/src/main/java/com/example/cupcakeapp/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcakeapp/CupcakeScreen.kt
@@ -1,28 +1,21 @@
 package com.example.cupcakeapp
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cupcakeapp.ui.theme.CupcakeAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CupcakeApp() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-        )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Cupcake") }
+            )
+        }
+    ) { innerPadding ->
+        // Por ahora solo estamos configurando la TopAppBar
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
